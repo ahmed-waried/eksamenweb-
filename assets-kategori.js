243741c7be0d2aa5cfaabc444006b5cfc33d23c2
ahmed-waried <ahmedmw@hiof.no>
2024-11-29 01:18:12 +0100

    update 3

---
+++ assets-kategori.js
@@ -0,0 +1,26 @@
+// Asset liste for prosjektet
+
+const assets = {
+    images: [
+        {
+            id: 1,
+            src: "assets/nike-5418992_1280.jpg",
+            alt: "Sporty Nike-sko svevende over hender",
+            credit: "Image by Mohamed Marzuk Mohamed Ansari from Pixabay",
+        },
+        {
+            id: 2,
+            src: "assets/ball-7610545_1280.jpg",
+            alt: "Basketballer fra Spalding i nærbilde",
+            credit: "Image by Schorsch from Pixabay",
+        },
+        {
+            id: 3,
+            src: "assets/boy-5691813_1920.jpg",
+            alt: "Gutt i sportsantrekk med ball mot blå himmel",
+            credit: "Image by Steven Weirather from Pixabay",},
+    ],
+};
+
+// Eksporter listen for bruk i andre deler av prosjektet
+export default assets;
