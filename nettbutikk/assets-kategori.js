e39f84e9bcf5c112a3f683223799a5aaa69c5d72
ahmed-waried <ahmedmw@hiof.no>
2024-11-29 08:39:43 +0100

    29/9/24

--- nettbutikk/assets-kategori.js
+++ nettbutikk/assets-kategori.js
@@ -22,5 +22,9 @@ const assets = {
     ],
 };
 
+https://cdn.pixabay.com/photo/2020/07/19/05/31/nike-5418992_1280.jpg
+https://cdn.pixabay.com/photo/2022/11/22/20/25/ball-7610545_1280.jpg
+https://cdn.pixabay.com/photo/2020/10/27/20/18/boy-5691813_1280.jpg
+
 // Eksporter listen for bruk i andre deler av prosjektet
 export default assets;
